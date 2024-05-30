559275d3a2967fc2bc1f8a5db92c5dfbb7650f8d
Dhruv <adhruv674@gmail.com>
2024-05-30 10:12:35 +0530

    made course sidebar responsive

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -3,15 +3,18 @@ import { sidebarLinks } from '../../../data/dashboard-links'
 import { logout } from '../../../services/operations/authAPI'
 import { useDispatch, useSelector } from 'react-redux'
 import { SidebarLink } from './SidebarLink'
-import { CiSettings } from "react-icons/ci";
 import { VscSignOut } from "react-icons/vsc"
 import { useNavigate } from 'react-router-dom'
 import { ConfirmationModal } from '../../common/ConfirmationModal'
+import { AiOutlineMenu } from "react-icons/ai";
+import { RxDropdownMenu } from "react-icons/rx";
+
 export const Sidebar = () => {
 
     const { user, loading: profileLoading } = useSelector((state) => state.profile);
     const { loading: authLoading } = useSelector((state) => state.auth);
     const [confirmationModal, setConfirmationModal] = useState(null);
+    const [open, setOpen] = useState(false);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -23,8 +26,9 @@ export const Sidebar = () => {
         )
     }
     return (
-        <div >
-            <div className='flex h-[calc(100vh-3.5rem)] min-w-[220px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800 py-10'>
+        <div>
+            {/* for laptop screen */}
+            <div className='lg:flex lg:h-[calc(100vh-3.5rem)] pb-10 min-w-[220px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800 py-10 hidden'>
                 <div className='flex flex-col'>
                     {
                         sidebarLinks.map((link, index) => {
@@ -66,6 +70,69 @@ export const Sidebar = () => {
                     </button>
                 </div>
             </div>
+
+            {/* for small screen */}
+            <div className='flex justify-center lg:hidden'>
+                <div className='h-[24px] w-[29px] bg-richblack-600 rounded-b-xl relative' ></div>
+                {
+                    !open &&
+                    <AiOutlineMenu className='text-white text-center absolute' onClick={() => {
+                        setOpen(!open);
+                    }} />
+                }
+                {
+                    open &&
+                    <RxDropdownMenu className='text-white text-center absolute' onClick={() => {
+                        setOpen(!open);
+                    }} />
+
+                }
+            </div>
+            {
+                open &&
+                <div className='flex lg:h-[calc(100vh-3.5rem)] pb-10 min-w-[220px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800 py-10 lg:hidden'>
+                    <div className='flex flex-col'>
+                        {
+                            sidebarLinks.map((link, index) => {
+                                if (link.type && user?.accountType !== link.type) return null;
+                                return (
+                                    <SidebarLink key={link.id} link={link} iconName={link.icon} />
+                                )
+                            })
+                        }
+                    </div>
+                    <div className='mx-auto mt-6 mb-6 h-[1px] w-10/12 bg-richblack-700"'></div>
+
+                    <div className='flex flex-col'>
+                        <SidebarLink
+                            link={{
+                                name: "Settings",
+                                path: "dashboard/settings"
+                            }}
+                            iconName="VscSettingsGear"
+                        />
+
+                        <button
+                            onClick={() => setConfirmationModal({
+                                text1: "Are You Sure ?",
+                                text2: "You will be logged out of your Account",
+                                btn1Text: "Logout",
+                                btn2Text: "Cancel",
+                                btn1Handler: () => dispatch(logout(navigate)),
+                                btn2Handler: () => setConfirmationModal(null),
+                            })}
+                            className='px-8 py-2 text-sm font-medium text-richblack-300'
+                        >
+
+                            <div className='flex items-center gap-x-2'>
+                                <VscSignOut className='text-lg' />
+                                <span>Logout</span>
+                            </div>
+
+                        </button>
+                    </div>
+                </div>
+            }
             {confirmationModal && <ConfirmationModal modalData={confirmationModal} className="z-10" />}
         </div>
     )
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,7 +15,7 @@ export const Dashboard = () => {
     }
 
     return (
-        <div className='relative flex min-h-[calc(100vh-3.5rem)]'>
+        <div className='relative flex min-h-[calc(100vh-3.5rem)] lg:flex-row flex-col'>
             <Sidebar />
             <div className='h-[calc(100vh-3.5rem)] flex-1 overflow-auto'>
                 <div className='mx-auto w-11/12 max-w-[1000px] py-10'>
